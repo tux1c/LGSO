83a98037dd98dd42e0aa3fd80688d2c5fb52323c
Tux1c <Tux1c@users.noreply.github.com>
2014-10-16 18:06:46 +0300

    Uploaded v1.2
    Added XDG support. Added backup function. Made the code more readable. LGSO will now check if it copied the directory succesfully before removing the old one.

--- lgso.sh
+++ lgso.sh
@@ -10,20 +10,81 @@ NEW_DIR=""
 OLD_DIR=""
 
 COUNTER=0
+MOVED=0
 OUTPUT=0
 BACKUP=0
 RESTORE=0
 
-function check_update {
+
+main() {
+   curl -s https://raw.githubusercontent.com/Tux1c/Tux1c.github.io/master/projfiles/lgso/version.txt | while read line; do
+      check_update $line
+   done
+
+   check_XDG
+   read_flags "$@"
+
+   if [[ $RESTORE -eq 1 ]]; then
+      restore
+   fi
+
+   # Checks if SRC_DIR exists, if not, creates it.
+   if [ ! -d "$SRC_DIR" ]; then
+      mkdir -p $SRC_DIR
+   fi
+
+   if [[ $OUTPUT -ne -1 ]]; then
+      echo "LGSO is now organizing your save files..."
+   fi
+
+   # Reads line by line from the online database.
+   curl -s https://raw.githubusercontent.com/Tux1c/Tux1c.github.io/master/projfiles/lgso/lgsolist.txt | while read line; do
+
+      # Increases counter - needed to determine if the vars are ready to work with.
+      let COUNTER=COUNTER+1
+
+      # Checks if line is a name of a game.
+      if [[ $line == *#* ]]; then
+         NEW_DIR=$SRC_DIR/${line:2}
+      # Else, it will assume the line is a location of the game save.
+      else
+        OLD_DIR=$HOME$line
+      fi
+
+      # Runs check if: variables are ready to work with && LGSO wasn't applied to specific directory. Then creates a new dir (if needed), moves the files and creates a new symlink.
+      if [ $((COUNTER%2)) -eq 0 ]; then
+         if [ -d "$OLD_DIR" ] && [ ! -L "$OLD_DIR" ]; then
+            move_save
+         fi
+      fi
+   done
+
+   if [[ $OUTPUT -ne -1 ]]; then
+      echo "LGSO has moved $MOVED games".
+   fi
+
+   if [[ $BACKUP -eq 1 ]]; then
+      backup
+   fi
+}
+
+check_update() {
    if [[ $(echo "$version < $1"|bc) -eq 1 ]]; then
       echo "Your LGSO version is outdated!"
       echo "You are using LGSO $version while the most recent version is $line"
       echo "It is important for you to keep this script up to date!"
       echo "Please visit https://github.com/Tux1c/LGSO and update to the latest version!"
+      exit 1
    fi
 }
 
-function read_flags {
+check_XDG() {
+   if [[ ! -z "$var" ]]; then
+      SRC_DIR=$XDG_DATA_HOME/games
+   fi
+}
+
+read_flags() {
    for i in $*; do
       if [ $i == -s ] || [ $i == -silent ]; then
          OUTPUT=-1
@@ -37,18 +98,64 @@ function read_flags {
          RESTORE=1 
       elif [[ $i == *-* ]]; then
          echo "Unknown parameter $i, aborting."
-         exit -1
+         exit 1
       fi
    done
 }
 
-function backup {
-   if [[ $OUTPUT -ne -1 ]]; then
-      echo "Backing up"
+move_save() {
+   if [ ! -d "$NEW_DIR" ]; then
+      if [[ $OUTPUT -eq 1 ]]; then
+         echo "Source path: $OLD_DIR"
+         echo "Destination path: $NEW_DIR"
+      fi
+
+      if [[ $OUTPUT -eq 1 ]]; then
+         echo "Creating $NEW_DIR"
+      fi
+
+      mkdir $NEW_DIR
    fi
+
+   if [[ $OUTPUT -eq 1 ]]; then
+      echo "Moving $OLD_DIR to $NEW_DIR"
+   fi
+
+   rm -rf $NEW_DIR/
+   cp -r $OLD_DIR/. $NEW_DIR
+
+   if verify_cp; then
+      if [[ $OUTPUT -eq 1 ]]; then
+         echo "Creating symlink in $OLD_DIR to $NEW_DIR"
+      fi 
+      rm -rf $OLD_DIR
+      ln -s $NEW_DIR $OLD_DIR
+   else
+      echo "Failed to move $OLD_DIR, retrying!"
+      move_save
+   fi
+
+   let MOVED=MOVED+1
 }
 
-function restore {
+verify_cp() {
+   diff $f $Dir2 > /dev/null 2>&1
+   return $?
+}
+
+backup() {
+   if [[ $OUTPUT -ne -1 ]]; then
+      echo "LGSO will now backup your save files"
+   fi
+   if [ -f $SRC_DIR/backup.tar.gz ]; then
+      rm $SRC_DIR/backup_old.tar.gz > /dev/null 2>&1
+      mv $SRC_DIR/backup.tar.gz $SRC_DIR/backup_old.tar.gz
+   fi
+
+   tar czf $SRC_DIR/backup.tar.gz $SRC_DIR/* > /dev/null 2>&1
+}
+
+restore() {
    if [[ $OUTPUT -ne -1 ]]; then
       echo "Restoring"
    fi
@@ -57,73 +164,6 @@ function restore {
 }
 
 
-### Actual Code ###
 
-curl -s https://raw.githubusercontent.com/Tux1c/Tux1c.github.io/master/projfiles/lgso/version.txt | while read line; do
-   check_update $line
-done
-
-read_flags
-
-if [[ $RESTORE -eq 1 ]]; then
-   restore
-fi
-
-# Checks if SRC_DIR exists, if not, creates it.
-if [ ! -d "$SRC_DIR" ]; then
-   mkdir $SRC_DIR
-fi
-
-if [[ $OUTPUT -ne -1 ]]; then
-   echo "LGSO is now organizing your save files..."
-fi
-
-# Reads line by line from the online database.
-curl -s https://raw.githubusercontent.com/Tux1c/Tux1c.github.io/master/projfiles/lgso/lgsolist.txt | while read line; do
-
-   # Increases counter - needed to determine if the vars are ready to work with.
-   let COUNTER=COUNTER+1
-
-   # Checks if line is a name of a game.
-   if [[ $line == *#* ]]; then
-      NEW_DIR=$SRC_DIR/${line:2}
-      if [[ $OUTPUT -eq 1 ]]; then
-         echo "Destination path: $NEW_DIR"
-      fi
-   # Else, it will assume the line is a location of the game save.
-   else
-     OLD_DIR=$HOME$line
-     if [[ $OUTPUT -eq 1 ]]; then
-        echo "Source path: $OLD_DIR"
-     fi
-   fi
-
-   # Runs check if: variables are ready to work with && LGSO wasn't applied to specific directory. Then creates a new dir (if needed), moves the files and creates a new symlink.
-   if [ $((COUNTER%2)) -eq 0 ]; then
-      if [ -d "$OLD_DIR" ] && [ ! -L "$OLD_DIR" ]; then
-         if [ ! -d "$NEW_DIR" ]; then
-            if [[ $OUTPUT -eq 1 ]]; then
-               echo "Creating $NEW_DIR"
-            fi
-            mkdir $NEW_DIR
-         fi
-         if [[ $OUTPUT -eq 1 ]]; then
-            echo "Moving $OLD_DIR to $NEW_DIR"
-         fi
-         cp -r $OLD_DIR/. $NEW_DIR
-         if [[ $OUTPUT -eq 1 ]]; then
-            echo "Creating symlink in $OLD_DIR to $NEW_DIR"
-         fi
-         rm -rf $OLD_DIR
-         ln -s $NEW_DIR $OLD_DIR
-      fi
-   fi
-done
-
-if [[ $OUTPUT -ne -1 ]]; then
-   echo "LGSO has moved $((COUNTER/2)) games".
-fi
-
-if [[ $BACKUP -eq 1 ]]; then
-   backup
-fi
+# Runs LGOS
+main "$@"
