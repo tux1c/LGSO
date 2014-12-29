7c75bfd0278001ee175b0e7f20dd8f82b2a047f5
Adam Mizerski <adam@mizerski.pl>
2014-12-30 00:06:52 +0100

    fixed quoting strings, fixed verify_cp

--- lgso.sh
+++ lgso.sh
@@ -18,22 +18,22 @@ RESTORE=0
 
 main() {
    curl -s https://raw.githubusercontent.com/Tux1c/Tux1c.github.io/master/projfiles/lgso/version.txt | while read line; do
-      check_update $line
+      check_update "$line"
    done
 
    check_XDG
    read_flags "$@"
 
-   if [[ $RESTORE -eq 1 ]]; then
+   if [[ "$RESTORE" -eq 1 ]]; then
       restore
    fi
 
    # Checks if SRC_DIR exists, if not, creates it.
    if [ ! -d "$SRC_DIR" ]; then
-      mkdir -p $SRC_DIR
+      mkdir -p "$SRC_DIR"
    fi
 
-   if [[ $OUTPUT -ne -1 ]]; then
+   if [[ "$OUTPUT" -ne -1 ]]; then
       echo "LGSO is now organizing your save files..."
    fi
 
@@ -44,11 +44,11 @@ main() {
       let COUNTER=COUNTER+1
 
       # Checks if line is a name of a game.
-      if [[ $line == *#* ]]; then
-         NEW_DIR=$SRC_DIR/${line:2}
+      if [[ "$line" =~ ^# ]]; then
+         NEW_DIR="${SRC_DIR}/${line:2}"
       # Else, it will assume the line is a location of the game save.
       else
-        OLD_DIR=$HOME$line
+        OLD_DIR="${HOME}${line}"
       fi
 
       # Runs check if: variables are ready to work with && LGSO wasn't applied to specific directory. Then creates a new dir (if needed), moves the files and creates a new symlink.
@@ -59,11 +59,11 @@ main() {
       fi
    done
 
-   if [[ $OUTPUT -ne -1 ]]; then
+   if [[ "$OUTPUT" -ne -1 ]]; then
       echo "LGSO has moved $MOVED games".
    fi
 
-   if [[ $BACKUP -eq 1 ]]; then
+   if [[ "$BACKUP" -eq 1 ]]; then
       backup
    fi
 }
@@ -79,8 +79,8 @@ check_update() {
 }
 
 check_XDG() {
-   if [[ ! -z "$XDG_DATA_HOME" ]]; then
-      SRC_DIR=$XDG_DATA_HOME/games
+   if [[ -n "$XDG_DATA_HOME" ]]; then
+      SRC_DIR="$XDG_DATA_HOME/games"
    fi
 }
 
@@ -101,32 +101,32 @@ read_flags() {
 }
 
 move_save() {
-   if [ ! -d "$NEW_DIR" ]; then
-      if [[ $OUTPUT -eq 1 ]]; then
+   if [[ ! -d "$NEW_DIR" ]]; then
+      if [[ "$OUTPUT" -eq 1 ]]; then
          echo "Source path: $OLD_DIR"
          echo "Destination path: $NEW_DIR"
       fi
 
-      if [[ $OUTPUT -eq 1 ]]; then
+      if [[ "$OUTPUT" -eq 1 ]]; then
          echo "Creating $NEW_DIR"
       fi
 
-      mkdir $NEW_DIR
+      mkdir "$NEW_DIR"
    fi
 
-   if [[ $OUTPUT -eq 1 ]]; then
+   if [[ "$OUTPUT" -eq 1 ]]; then
       echo "Moving $OLD_DIR to $NEW_DIR"
    fi
 
-   rm -rf $NEW_DIR/
-   cp -r $OLD_DIR/. $NEW_DIR
+   rm -rf "$NEW_DIR/"
+   cp -r "$OLD_DIR/." "$NEW_DIR"
 
-   if verify_cp; then
-      if [[ $OUTPUT -eq 1 ]]; then
+   if verify_cp "$NEW_DIR" "$OLD_DIR"; then
+      if [[ "$OUTPUT" -eq 1 ]]; then
          echo "Creating symlink in $OLD_DIR to $NEW_DIR"
-      fi 
-      rm -rf $OLD_DIR
-      ln -s $NEW_DIR $OLD_DIR
+      fi
+      rm -rf "$OLD_DIR"
+      ln -s "$NEW_DIR" "$OLD_DIR"
    else
       echo "Failed to move $OLD_DIR, retrying!"
       move_save
@@ -136,31 +136,29 @@ move_save() {
 }
 
 verify_cp() {
-   diff $f $Dir2 > /dev/null 2>&1
+   diff -qr "$1" "$2" > /dev/null 2>&1
    return $?
 }
 
 backup() {
-   if [[ $OUTPUT -ne -1 ]]; then
+   if [[ "$OUTPUT" -ne -1 ]]; then
       echo "LGSO will now backup your save files"
    fi
-   if [ -f $SRC_DIR/backup.tar.gz ]; then
-      rm $SRC_DIR/backup_old.tar.gz > /dev/null 2>&1
-      mv $SRC_DIR/backup.tar.gz $SRC_DIR/backup_old.tar.gz
+   if [ -f "$SRC_DIR/backup.tar.gz" ]; then
+      rm "$SRC_DIR/backup_old.tar.gz" > /dev/null 2>&1
+      mv "$SRC_DIR/backup.tar.gz" "$SRC_DIR/backup_old.tar.gz"
    fi
 
-   tar czf $SRC_DIR/backup.tar.gz $SRC_DIR/* > /dev/null 2>&1
+   tar czf "$SRC_DIR/backup.tar.gz" $SRC_DIR/* > /dev/null 2>&1
 }
 
 restore() {
-   if [[ $OUTPUT -ne -1 ]]; then
+   if [[ "$OUTPUT" -ne -1 ]]; then
       echo "Restoring"
    fi
 
    exit 0
 }
-
-
 
 # Runs LGOS
 main "$@"
